b6cf1546d2e50594631f4430506b1140efe3ef1e
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-10-06 21:19:46 +0200

    working

--- main/main.c
+++ main/main.c
@@ -62,6 +62,55 @@ mcp9808_err_t mcp9808_bus_read_data(void* user,
     return MCP9808_ERR_OK;
 }
 
+static mcp9808_err_t mcp9808_initialize_chip(mcp9808_t const* mcp9808)
+{
+    mcp9808_err_t err = MCP9808_ERR_OK;
+
+    mcp9808_manufacturer_id_reg_t man_id = {0};
+    mcp9808_device_id_reg_t dev_id = {0};
+
+    err |= mcp9808_get_manufacturer_id_reg(mcp9808, &man_id);
+    err |= mcp9808_get_device_id_reg(mcp9808, &dev_id);
+
+    if (err != MCP9808_ERR_OK)
+        return err;
+
+    if (man_id.manufacturer_id != 0x0054 || (dev_id.device_id & 0xFF) != 0x04) {
+        return MCP9808_ERR_FAIL;
+    }
+
+    mcp9808_config_reg_t cfg = {0};
+    err |= mcp9808_get_config_reg(mcp9808, &cfg);
+
+    cfg.t_hyst = 0;
+    cfg.shdn = 0;
+    cfg.crit_lock = 0;
+    cfg.win_lock = 0;
+    cfg.int_clear = 0;
+    cfg.alert_stat = 0;
+    cfg.alert_cnt = 0;
+    cfg.alert_sel = 0;
+    cfg.alert_pol = 0;
+    cfg.aler_mod = 0;
+
+    err |= mcp9808_set_config_reg(mcp9808, &cfg);
+
+    mcp9808_resolution_reg_t res = {.resolution = 0x03};
+    err |= mcp9808_set_resolution_reg(mcp9808, &res);
+
+    mcp9808_t_upper_reg_t upper = {.t_upper = 30 << 4};
+    mcp9808_t_lower_reg_t lower = {.t_lower = 20 << 4};
+    mcp9808_t_crit_reg_t crit = {.t_crit = 40 << 4};
+
+    err |= mcp9808_set_t_upper_reg(mcp9808, &upper);
+    err |= mcp9808_set_t_lower_reg(mcp9808, &lower);
+    err |= mcp9808_set_t_crit_reg(mcp9808, &crit);
+
+    return err;
+}
+
+float32_t mcp9808_resolution_to_scale(mcp9808_resolution_t);
+
 int main(void)
 {
     HAL_Init();
@@ -76,7 +125,8 @@ int main(void)
         .i2c_address = MCP9808_SLAVE_ADDRESS_A2L_A1L_A0L << 1};
 
     mcp9808_t mcp9808;
-    mcp9808_config_t mcp9808_config = {.scale = 0.5F};
+    mcp9808_config_t mcp9808_config = {.scale =
+                                           mcp9808_resolution_to_scale(0x03)};
     mcp9808_interface_t mcp9808_interface = {
         .bus_user = &mcp9808_user,
         .bus_initialize = mcp9808_bus_initialize,
@@ -85,11 +135,16 @@ int main(void)
 
     if (mcp9808_initialize(&mcp9808, &mcp9808_config, &mcp9808_interface) !=
         MCP9808_ERR_OK) {
-        Error_Handler();
+        printf("Error!\n\r");
+    }
+
+    if (mcp9808_initialize_chip(&mcp9808) != MCP9808_ERR_OK) {
+        printf("Error!\n\r");
     }
 
     float scaled;
     while (1) {
         mcp9808_get_temp_data_scaled(&mcp9808, &scaled);
+        printf("scaled: %f\n\r", scaled);
     }
 }
